3342b6306875529329ccbd76fe4182e916c5cb89
himansh <hssharma2212000@gmail.com>
2019-11-27 19:13:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom'; 
 import logo from './logo.svg';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Details from './components/Details';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
+
 import Default from './components/Default';
 
 import './App.css';
@@ -17,11 +18,16 @@ class App extends Component {
       <React.Fragment>
      <h3> hello for app</h3>
      <Navbar />
-     <ProductList />
-     <Details />
-     <Cart />
-     <Default />
-      
+     <Switch>
+     <Route path='/' Component={ProductList}></Route>
+     <Route path='/details' Component={Details}></Route>
+     <Route path='/cart' Component={Cart}></Route>
+     <Route compponent={Default}></Route>
+     </Switch>
+     
+  
+
+    
       
       
       </React.Fragment>
