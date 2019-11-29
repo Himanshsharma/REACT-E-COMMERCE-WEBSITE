eda57c92faf2de81c81defc3a0c7a8ae6032534f
himansh <hssharma2212000@gmail.com>
2019-11-29 18:02:51 +0530

    title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-export default function Title(name,title){
+export default function Title({name,title}){
     return(
         <div className="row">
         <div className="mx-auto col-10 text-title my-2 text-center">
         <h1 className="text-capitalise font-weight-bold">
-        {name} <strong className="text-Blue">{title}</strong></h1>
+        {name} <strong className="text-blue">{title}</strong></h1>
         
         </div>
         </div>
