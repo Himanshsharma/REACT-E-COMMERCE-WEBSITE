c8d22538ae60b3211002731202bb9919aaebb566
himansh <hssharma2212000@gmail.com>
2019-12-05 05:40:25 +0530

    transform

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,7 +13,8 @@ class Product extends Component {
             <div className="img-container p-5" onClick={()=>console.log("you")}>
             <Link to="/details">
             <img src={img} alt="product" className="card-img-top"></img></Link>
-            <button className='cart-btn' disabled={inCart ? true : false}>
+            <button className='cart-btn' disabled={inCart ? true : false} onClick={()=>{console.log("lololo")}}>
+            { inCart ? <p className='text-capitalize mb-0' disabled>in in cart</p>: <i className='fas fa-cart-plus'></i>}
             
             
             
@@ -22,12 +23,52 @@ class Product extends Component {
             
             
             </div>
-            </div>
-            </ProductWrapper>
+{/* card footer */}
+
+<div className="card-footer d-flex justify-content-between">
+<p className="align-self-center mb-0">{title}</p><h5 className="text-blue font-italic mb-0">
+<span className="mr-1">
+$
+</span>
+{price}
+</h5></div>
+</div>
+</ProductWrapper>
          );
     }
 }
  const ProductWrapper = styled.div`
+.card{
+    border-color:transparent;
+    transition:all is linear;
+
+}
+.card-footer{
+    background:transparent;
+    border-top:transparent;
+    transition:all is linear;
+}
+&:hover{
+    .card{
+        border:0.4ren solid rgba(0,0,0,2);
+        box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2);
+
+    }
+    .card-footer{
+        background:rgba(247,247,247);
+    }
+}
+.img-container{
+    position:relative;
+overflow:hidden;
+}
+.card-img-top{
+    transition:all is linear;
+}
+
+.img-container:hover .card-img-top{
+transform:scale(1.2);
+}
 
 `
 export default Product;
