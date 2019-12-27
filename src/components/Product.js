e4bb48f7b454b1ef011fdac2e33c61659e94a7d9
himansh <hssharma2212000@gmail.com>
2019-12-28 00:49:31 +0530

    proptypes

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Link } from 'react-router-dom';
 import {ProductConsumer} from '../context';
+import PropTypes from 'prop-types';
 class Product extends Component {
     state = {  }
 
@@ -37,6 +38,20 @@ $
          );
     }
 }
+Product.propTypes={
+product:PropTypes.shape({
+    id:PropTypes.number,
+    img:PropTypes.string,
+    title:PropTypes.string,
+    price:PropTypes.number,
+    inCart:PropTypes.bool,
+})
+
+
+
+}
+
+
  const ProductWrapper = styled.div`
 .card{
     border-color:transparent;
