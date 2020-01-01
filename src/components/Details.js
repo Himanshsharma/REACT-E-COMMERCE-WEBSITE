c06889dcbc2593baabd9182c95198a08e0026fb6
himansh <hssharma2212000@gmail.com>
2020-01-01 22:01:58 +0530

    details

--- src/components/Details.js
+++ src/components/Details.js
@@ -10,12 +10,42 @@ class Details extends Component {
         return ( 
         <ProductConsumer>
         {(value)=>{
-            console.log(value.detailProduct);
+            const { id,company,img,info,price,title,inCart }=value.detailProduct;
+            return(
+                <div className="container py-5">
+                {/* title */ }
+                <div className='row'>
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                <h1>{title}</h1>
+            </div>
+                </div>
+                {/* end title */}
+                {/* product info */}
+                <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                <img src={img} className="img-fluid" alt="product" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                <h1>Model : {title}</h1>
+                <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                made by : <span className="text-uppercase">
+                {company}</span>
+                </h4>
+                <h4 className="text-blue">
+                <strong>
+                price:<span>$</span>
+                {price}
+                </strong>
+                
+                </h4>
+                
+                
+                
+                
+                </div></div>
+                </div>
+            )
         }}
-        
-        
-        
-        
         </ProductConsumer>    
             
             
