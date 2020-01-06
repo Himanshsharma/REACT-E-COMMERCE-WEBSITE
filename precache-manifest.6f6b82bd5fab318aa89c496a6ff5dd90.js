069a5d3bcd26f35609220227e00545301cd6c5f2
himansh <hssharma2212000@gmail.com>
2020-01-06 11:18:48 +0530

    Updates

---
+++ precache-manifest.6f6b82bd5fab318aa89c496a6ff5dd90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec7d6e175854b4d71d63af1075818a5",
+    "url": "/Himanshsharma/REACT-E-COMMERCE-WEBSITE/index.html"
+  },
+  {
+    "revision": "9a7f8421aa2fc914d860",
+    "url": "/Himanshsharma/REACT-E-COMMERCE-WEBSITE/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "ade1d5648323bf6a7a76",
+    "url": "/Himanshsharma/REACT-E-COMMERCE-WEBSITE/static/css/main.68ecf83f.chunk.css"
+  },
+  {
+    "revision": "9a7f8421aa2fc914d860",
+    "url": "/Himanshsharma/REACT-E-COMMERCE-WEBSITE/static/js/2.6f8fbb49.chunk.js"
+  },
+  {
+    "revision": "ade1d5648323bf6a7a76",
+    "url": "/Himanshsharma/REACT-E-COMMERCE-WEBSITE/static/js/main.0c821a41.chunk.js"
+  },
+  {
+    "revision": "dce5c189671d96d0e2e3",
+    "url": "/Himanshsharma/REACT-E-COMMERCE-WEBSITE/static/js/runtime-main.68d5ded8.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/Himanshsharma/REACT-E-COMMERCE-WEBSITE/static/media/logo.9f7e506d.svg"
+  }
+]);
